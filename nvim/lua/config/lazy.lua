0d56430b6e50554b2dcfe916d48246be866a919d
Fábio Rodrigues <56126547+FabioRodrigues0@users.noreply.github.com>
2025-02-23 19:33:11 +0000

    Update lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -10,7 +10,6 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "pylint.vim" },
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
